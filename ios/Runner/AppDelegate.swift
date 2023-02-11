27d6ab773c72fcb59437e3715f89784c180c2479
zhangle <zhangle@nextmar.com>
2023-02-11 10:08:59 +0800

    设置安卓 iOS启动图

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,17 @@
 import UIKit
 import Flutter
 
+
+//启动图尺寸：
+//
+//选 640x1136、750x1334、1242x2688、分别加入1x、2x、3x里
+//
+//第一个是图片的名字，LaunchScreen 第二个是填充方式 Aspect fill
+
+//logo尺寸
+//
+//40x40    60x60  58x58  87x87    80x80  120x120  180x180  1024x1024
+
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
